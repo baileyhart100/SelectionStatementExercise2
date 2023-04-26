740377595bbd75431f4681a3380d4670a14feca4
Bailey Hart <baileyhart100@gmail.com>
2023-04-26 12:54:12 -0400

    initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough subject");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is a tough sibject");
+                    break;
+                case "english":
+                    Console.WriteLine("English is a tough subject");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I haven't taken that subject before. {subject} sounds fun!");
+                    break;
+                   
+            }
         }
     }
 }
